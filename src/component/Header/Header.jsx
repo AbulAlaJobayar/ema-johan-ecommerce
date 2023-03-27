8a141d5ee4c1578bf3fd8fdab7f7fe89d1a45ece
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-03-27 22:34:46 +0600

    first commit

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='header'>
+           <img src={logo} alt="" /> 
+        </div>
+    );
+};
+
+export default Header;
