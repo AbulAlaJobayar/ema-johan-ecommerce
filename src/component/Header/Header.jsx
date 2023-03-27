632d19ae1a7b327bed81df51b90885af4eba3527
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-03-27 22:51:14 +0600

    nav-aded

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,9 +4,15 @@ import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div className='header'>
+        <nav className='header'>
            <img src={logo} alt="" /> 
-        </div>
+           <div>
+            <a href="/Order">Order</a>
+            <a href="/Order Review">Order Review</a>
+            <a href="/Manage Inventory">Manage Inventory</a>
+            <a href="Login">Login</a>
+           </div>
+        </nav>
     );
 };
 
