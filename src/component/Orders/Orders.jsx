14632484deeaa0219bbfbc5f38f28237d136eff8
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-04-07 11:37:09 +0600

    order-added

--- src/component/Orders/Orders.jsx
+++ src/component/Orders/Orders.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart.jsx';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReveiwItem/ReviewItem.jsx';
+import "./Orders.css"
+import { removeFromDb } from '../../utilities/fakedb.js';
 
 const Orders = () => {
-    const cart = useLoaderData()
+    const savedCart = useLoaderData()
+    const [cart,setCart]=useState(savedCart)
+    const handleRemoveFromCart=(id)=>{
+        const remaining=cart.filter(product=>product.id!==id);
+        setCart(remaining);
+        removeFromDb(id)
+    }
+
     return (
         <div className='shop-container'>
-            <div className='product-container'>
-                <h3>orders-page</h3>
+            <div className='reveiw-container'>
+            {
+            cart.map(product=><ReviewItem product={product} key={product.id} handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
+            }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
